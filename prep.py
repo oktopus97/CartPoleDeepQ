abc8774429d7393911254e73fbf4fc45349eebbf
oktopus97 <reisbaltaoglu@gmail.com>
2019-11-30 15:22:21 +0100

    insert commit message

--- prep.py
+++ prep.py
@@ -3,7 +3,7 @@ prep.py:
 
     file for pre-processing data.
 """
-count= 0 
+count= 0
 from hyper_params import BATCH_SIZE, GAMMA
 from torch.autograd import Variable
 
@@ -12,8 +12,12 @@ import torch.cuda
 import numpy as np
 
 from random import randrange
-device = torch.device("cuda")# if torch.cuda.is_available() else "cpu")
-##is_available is not working with my current hardware :(
+if torch.cuda.is_available():
+    print('cuda')
+    torch.cuda.init()
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    device = torch.device('cuda')
+
 
 def prep_mem_batch(transitions):
     """
@@ -21,23 +25,23 @@ def prep_mem_batch(transitions):
 
     """
 
+
     state_batch, action_batch, _state_batch, reward_batch = zip(*transitions)
-    state_batch    =  torch.tensor(state_batch).float()
-    action_batch    = torch.tensor(action_batch).float()
-    _state_batch   =  torch.tensor(_state_batch).float()
-    reward_batch    = torch.tensor(reward_batch).float()
+    state_batch    =  torch.tensor(state_batch).float().to(device)
+    action_batch    = torch.tensor(action_batch).float().to(device)
+    _state_batch   =  torch.tensor(_state_batch).float().to(device)
+    reward_batch    = torch.tensor(reward_batch).float().to(device)
     return state_batch,action_batch,_state_batch,reward_batch
 
 
 def prep_exploitation(state):
-    state = torch.from_numpy(state).float()
+    state = torch.from_numpy(state).float().to(device)
 
     return state.unsqueeze(0)
 
 
 def prep_exploration(action_space):
-    return torch.tensor(randrange(int(action_space)))
+    return torch.tensor(randrange(int(action_space))).to(device)
 
-def prep_q(max_next,reward_batch):
-    q =  max_next*GAMMA + reward_batch
-    return q
+def prep_q(max_next,reward):
+    return max_next*GAMMA + reward
